ca10e47bf4c8c11bfe0a501b9fd4d10973fc1057
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2019-10-01 15:48:27 +0200

    setup material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1)
+  },
+  input: {
+    display: "none"
+  }
+}));
 
 const App: React.FC = () => {
+  const classes = useStyles();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I AM BACK MF</p>
+        <Button variant="contained" className={classes.button}>
+          Default
+        </Button>
+
+        <Button variant="contained" color="primary" className={classes.button}>
+          Primary
+        </Button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
